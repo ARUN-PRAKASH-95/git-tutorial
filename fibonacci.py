04590392d2a11d04e572a1d867d7faf16f2026be
ARUN-PRAKASH-95 <arunprakash.fns@gmail.com>
2019-04-25 20:26:02 +0200

    added a new ile

---
+++ fibonacci.py
@@ -0,0 +1,17 @@
+n = int(input('Enter the no of fibonacci terms you want: '))
+
+def fibonacci(n):
+    a = 0
+    b = 1
+    print(a)
+
+    for i in range(n-1):
+        a = a+b
+        b = a-b
+        if a % 2 == 0 :
+
+fibonacci(n)
+#lsnflnsl
+#skdfksk
+#fowb;owuohf
+#orgoehgoh
